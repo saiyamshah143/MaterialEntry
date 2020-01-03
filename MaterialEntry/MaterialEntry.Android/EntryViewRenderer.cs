3dce1d51542602de16780fc6c608bc5ce4f06368
saiyamshah143 <59467843+saiyamshah143@users.noreply.github.com>
2020-01-03 13:45:38 +0530

    Add files via upload
    Initial Commit

---
+++ MaterialEntry/MaterialEntry.Android/EntryViewRenderer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MaterialEntry;
+using MaterialEntry.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryView), typeof(EntryViewRenderer))]
+namespace MaterialEntry.Droid
+{
+    [Obsolete]
+    public class EntryViewRenderer : ViewRenderer
+    {
+        global::Android.Views.View view;
+        global::Android.Widget.EditText editText;
+        EntryView entryView;
+        Activity activity;
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                entryView = e.NewElement as EntryView;
+            }
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+            try
+            {
+                SetupUserInterface();
+                SetupEventHandlers();
+                AddView(view);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(@"ERROR: ", ex.Message);
+            }
+        }
+
+        private void SetupEventHandlers()
+        {
+            editText.TextChanged += EditText_TextChanged;
+        }
+
+        private void EditText_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            entryView.Text = editText.Text;
+            Console.WriteLine("chanegd +" + entryView.Text);
+        }
+
+        void SetupUserInterface()
+        {
+            activity = this.Context as Activity;
+            view = activity.LayoutInflater.Inflate(Resource.Layout.EntryLayout, this, false);
+            editText = view.FindViewById<EditText>(Resource.Id.editText1);
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+            var msw = MeasureSpec.MakeMeasureSpec(r - l, MeasureSpecMode.Exactly);
+            var msh = MeasureSpec.MakeMeasureSpec(b - t, MeasureSpecMode.Exactly);
+            view.Measure(msw, msh);
+            view.Layout(0, 0, r - l, b - t);
+        }
+    }
+}
---
+++ MaterialEntry/MaterialEntry/EntryView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MaterialEntry
+{
+    public class EntryView : ContentView
+    {
+        public static readonly BindableProperty TextProperty =
+          BindableProperty.Create("Text", typeof(string), typeof(EntryView), null);
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
